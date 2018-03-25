1d445ad25c493dc8454aed0a4d1a611415fd5e9e
Lowellll <yejin2651@naver.com>
2018-03-26 00:41:57 +0900

    [10824]네 수
    10824번 문제

---
+++ java/FourNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FourNumber {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String numbers = sc.nextLine();
+		String[] numbers_array = numbers.split("\\s");
+		System.out.print(Long.parseLong(numbers_array[0]+numbers_array[1])+Long.parseLong(numbers_array[2]+numbers_array[3]));
+	}
+}
