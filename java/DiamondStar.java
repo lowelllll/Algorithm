704a9fa2814e7274a88d84abbfa2281491e620ca
lowelllll <yejin2651@naver.com>
2018-10-30 20:57:34 +0900

    [java]다이아몬드 별찍기

---
+++ java/DiamondStar.java
@@ -0,0 +1,54 @@
+package prec;
+
+import java.util.Scanner;
+
+public class DiamondStar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("다이아몬드의 크기를 입력하세요(홀수)>>");
+		int column = sc.nextInt();
+		int row = column;
+		
+		for(int i=1; i<=column; i++) {
+			// 1분할 
+			for(int j=row-i; j>0; j--) {
+				System.out.print(' ');
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			
+			// 2분할 
+			for(int j=1; j<i; j++) {
+				System.out.print('*');
+			}
+			for(int j=row-i; j>0; j--) {
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+		
+		for(int i=1; i<column; i++) {
+			// 3분할
+			for(int j=1; j<=i; j++) {
+				System.out.print(' ');
+			}
+			for(int j=1; j<=row-i; j++) {
+				System.out.print('*');
+			}
+			
+			//4분할 
+			for(int j=1; j<row-i; j++) {
+				System.out.print('*');
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
