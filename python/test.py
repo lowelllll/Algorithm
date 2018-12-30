ef9c81416ed338c872978d4c5cfce630d69a4906
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-12-30 22:41:48 +0900

    [backjoon_java] 스티커
    bottom up 방식으로 품

--- python/test.py
+++ python/test.py
@@ -1,36 +1,17 @@
-def solution(heights):
-    index_q = [1]
-    queue = [heights[0]]
-    result = [0 for _ in range(len(heights) + 1)]
+def solution(answers):
+    students = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    check = {'1':{'idx':1, 'correct':0},'2':{'idx':2, 'correct':0},'3':{'idx':3, 'correct':0}}
+    for idx, student in enumerate(students):
+        for a_idx, answer in enumerate(answers):
+            if student[a_idx%len(student)] == answer:
+                check[str(idx+1)]['correct'] += 1
 
-    for i in range(1, len(heights)):
-        while len(queue)!=0:
-            if heights[i] >= queue[-1]:
-                queue.pop(-1)
-                cnt = index_q.pop(-1)
-                if len(queue) == 0:
-                    result[cnt] = 0
-                    break
-                else:
-                    result[cnt] = index_q[-1]
-            else:
-                break
-        queue.append(heights[i])
-        index_q.append(i + 1)
+    scores = list(check.values())
+    max_correct = max(tuple((score['correct'] for score in scores)))
+    print(max_correct)
+    print(scores)
+
+    return [score['idx'] for score in scores if score['correct'] >= max_correct]
 
 
-    while len(queue) != 0:
-        queue.pop(-1)
-        cnt = index_q.pop(-1)
-        if len(queue) != 0:
-            result[cnt] = index_q[-1]
-        else:
-            result[cnt] = 0
-
-    result.pop(0)
-    return result
-
-# 입력값 〉	[1, 5, 3, 6, 7, 6, 5]
-# 기댓값 〉	[0, 0, 2, 0, 0, 5, 6]
-
-print(solution([1,5,3,6,7,6,5]))
+print(solution([1,3,2,4,2]))
